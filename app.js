136670948e49572a36be3e84f59476481413e718
codenoidea <70764393+codenoidea@users.noreply.github.com>
2021-02-02 23:30:58 +0900

    feat(#1): 설정

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const AutoLoad = require('fastify-autoload')
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+  fastify.register(require('fastify-helmet'));
+  fastify.register(
+    require('fastify-compress'), {
+      global: false
+    }
+  );
 
   // Do not touch the following lines
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'services'),
+    options: Object.assign({}, opts)
+  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
@@ -14,12 +24,12 @@ module.exports = async function (fastify, opts) {
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
-  })
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
-  })
+  });
 }
--- routes/users/index.js
+++ routes/users/index.js
@@ -2,6 +2,9 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return `this is an example: someSupport: ${fastify.someSupport()}`
+    const {
+      query
+    } = request;
+    reply.send(fastify.usersGet(query));
   })
 }
